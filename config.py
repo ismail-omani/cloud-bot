d6830554c012c30ffd51edd9095b40a6cbb1630f
Ismail <laoprog@gmail.com>
2026-05-07 15:42:08 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+from aiogram import *
+from aiogram.types import *
+from aiogram.enums import ParseMode
+from aiogram.filters import *
+import asyncio
+import os
+from aiogram.types.input_file import FSInputFile
+import shutil
+from aiogram.client.session.aiohttp import AiohttpSession
+
 PROXY_URL = "socks5://127.0.0.1:9050"
 session = AiohttpSession(proxy=PROXY_URL)
 
